f479e59a359f4775df174f6f5686bd53d3c3bc18
adikamanoharreddy <100589467+adikamanoharreddy@users.noreply.github.com>
2026-04-16 19:07:26 +0530

    commit
    conditional statements

---
+++ batch1java/src/batch1java/oopsexamples/conditionalStates/IfConditionalStatementExample.java
@@ -0,0 +1,21 @@
+package batch1java.oopsexamples.conditionalStates;
+
+public class IfConditionalStatementExample {
+
+	public static void main(String[] args) {
+		
+		if (10==1) {
+			
+			//
+			//++,--,*,/,%,<,>,==,
+			
+			System.out.println("if block ...........");
+			}else if (20==00){
+				System.out.println("else if block........");
+			}else {
+				System.out.println("else block........");
+			}
+		
+	}
+
+}
---
+++ batch1java/src/batch1java/oopsexamples/conditionalStates/SwitchConditionalExamples.java
@@ -0,0 +1,25 @@
+package batch1java.oopsexamples.conditionalStates;
+
+public class SwitchConditionalExamples {
+
+	public static void main(String[] args) {
+		
+		String day ="jan";
+		
+		switch(day) {
+		case "jan":
+			System.out.println("jan");
+		break;
+		case "2":
+			System.out.println("feb");
+		break;
+		case "3":
+			System.out.println("mar");
+		break;
+		default:
+			System.out.println("default.........");
+		
+		}
+		
+	}
+}
