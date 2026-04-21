3c75d6b4fcaf6a12553534f9d0d52d182bc681cd
adikamanoharreddy <100589467+adikamanoharreddy@users.noreply.github.com>
2026-04-21 06:50:22 +0530

    looping statements
    loops

---
+++ batch1java/src/batch1java/oopsexamples/conditionalStates/DoWhileLoopExample.java
@@ -0,0 +1,18 @@
+package batch1java.oopsexamples.conditionalStates;
+
+public class DoWhileLoopExample {
+	
+	public static void main(String[] args) {
+		int i = 10;
+		
+		do {
+			System.out.println("i===="+i);
+			i++;
+		
+		
+		}while(i<20);
+		System.out.println("bye");
+	
+	}
+
+}
---
+++ batch1java/src/batch1java/oopsexamples/conditionalStates/WhileloopExample.java
@@ -0,0 +1,15 @@
+package batch1java.oopsexamples.conditionalStates;
+
+public class WhileloopExample {
+
+	public static void main(String[] args) {
+		int i=5;
+		
+		while(i<7) {
+			System.out.println("hi.....");
+			i++;
+			
+		}
+		System.out.println("bye");
+	}
+}
